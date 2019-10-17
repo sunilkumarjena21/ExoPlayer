696db579dbdd5857bf9eb536ecc964b5227cdd62
sunil <sunil.jena@soprasteria.com>
2019-10-17 11:59:20 +0530

    [FIX] Sunil 1 log added.

--- demos/ima/src/main/java/com/google/android/exoplayer2/imademo/PlayerManager.java
+++ demos/ima/src/main/java/com/google/android/exoplayer2/imademo/PlayerManager.java
@@ -71,7 +71,7 @@ import com.google.android.exoplayer2.util.Util;
     player.seekTo(contentPosition);
     player.prepare(mediaSourceWithAds);
     player.setPlayWhenReady(true);
-    Log.e("Sunil","Sunil");
+    Log.e("Sunil","Sunil1");
   }
 
   public void reset() {
